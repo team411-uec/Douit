3c954abec647b9ed84364e83e1ccaeb34bba24f9
Tsukimi41 <masaaki.cool99@gmail.com>
2025-08-26 19:58:40 +0900

    規約に加えるボタン

---
+++ src/app/components/AddTermButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AddTermButton.css"; // スタイルを適用するためのCSSファイルをインポート
+
+// このコンポーネントが受け取るpropsの型を定義します
+type AddTermButtonProps = {
+  /** ボタンがクリックされたときに実行される関数 */
+  onClick: () => void;
+  /** ボタンを無効化するかどうか (任意) */
+  disabled?: boolean;
+};
+
+const AddTermButton = ({ onClick, disabled = false }: AddTermButtonProps) => {
+  return (
+    <button className="add-term-button" onClick={onClick} disabled={disabled}>
+      規約に加える
+    </button>
+  );
+};
+
+export default AddTermButton;
